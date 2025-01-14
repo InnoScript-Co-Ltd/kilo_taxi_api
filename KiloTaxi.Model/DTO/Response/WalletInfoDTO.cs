6c6306421636f186cfd2755eaa4cf402f1327905
Wai Tun Soe <waitunsoe.dev@gmail.com>
2025-01-15 02:18:44 +0630

    change response typ (wallet)

---
+++ KiloTaxi.Model/DTO/Response/WalletInfoDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace KiloTaxi.Model.DTO.Response;
+
+public class WalletInfoDTO
+{
+    public int Id { get; set; }
+
+    public string WalletName { get; set; }
+    
+    public DateTime CreateDate { get; set; }
+
+    public DateTime? UpdateDate { get; set; }
+}
--- KiloTaxi.Model/DTO/WalletPagingDTO.cs
+++ KiloTaxi.Model/DTO/WalletPagingDTO.cs
@@ -1,7 +1,9 @@
-﻿namespace KiloTaxi.Model.DTO;
+﻿using KiloTaxi.Model.DTO.Response;
+
+namespace KiloTaxi.Model.DTO;
 
 public class WalletPagingDTO
 {
     public PagingResult Paging { get; set; }
-    public IEnumerable<WalletDTO> Wallets { get; set; }
+    public IEnumerable<WalletInfoDTO> Wallets { get; set; }
 }
